2d877879f95593c41cd8c73a032dbc8a9782813e
liyan34 <liyan34@lenovo.com>
2023-05-17 17:10:23 +0800

    map loading

--- components/widgets/FMMapWidget.jsx
+++ components/widgets/FMMapWidget.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WebView from "react-native-webview";
-import { StyleSheet, View } from "react-native";
-// import mapHtml from '../../resource/fmmap/test.html';
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import mapHtml from "../../resource/fmmap/index";
 
-
 function FMMapWidget({ mapInfo, onMapReady, onWebViewRef }) {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <View style={styles.container}>
+      {isLoading && <LoadingView />}
       <WebView style={styles.webview}
         allowUniversalAccessFromFileURLs={true}
         allowingReadAccessToURL={true}
@@ -19,6 +20,8 @@ function FMMapWidget({ mapInfo, onMapReady, onWebViewRef }) {
         // nativeConfig={{ props: { webContentsDebuggingEnabled: true } }}
         javaScriptEnabled={true}
         scrollEnabled={false}
+        onLoadStart={() => { setIsLoading(true) }}
+        onLoadEnd={() => { setIsLoading(false) }}
         onMessage={event => {
           const message = event.nativeEvent.data;
           console.log("webview message received:", message)
@@ -33,6 +36,19 @@ function FMMapWidget({ mapInfo, onMapReady, onWebViewRef }) {
           }
         }}
       />
+    </View>
+  )
+}
+
+function LoadingView() {
+  return (
+    <View style={{ position: 'absolute', top: '38.2%', center: '50%', alignSelf: 'center', zIndex: 100, justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <View style={{
+        backgroundColor: 'rgba(0 0 0 / .618)', padding: 12, borderRadius: 6,
+      }}>
+        <ActivityIndicator></ActivityIndicator>
+        <Text style={{ marginTop: 8, color: '#fff' }}>正在载入地图</Text>
+      </View>
     </View>
   )
 }
